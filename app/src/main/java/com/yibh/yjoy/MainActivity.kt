97c92963c26a763fbcfd244ebf3a98e97af4539f
benhaoyi <benhaoyi@aijiatui.com>
2019-09-26 16:08:16 +0800

    导入组件化库

--- app/src/main/java/com/yibh/yjoy/MainActivity.kt
+++ app/src/main/java/com/yibh/yjoy/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.yibh.yjoy
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
